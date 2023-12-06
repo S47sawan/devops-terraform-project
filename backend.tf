ee66e6f2b933134d5fb563050cfa5cafdb71c1b2
smihah <smihah270@yahoo.com>
2023-12-06 17:53:11 +0000

    created Internet gateway

--- backend.tf
+++ backend.tf
@@ -3,10 +3,10 @@
 #####################################################################################################################
 terraform {
   backend "s3" {
-    bucket = "devops-terraform-state-smihah"
-    key    = "global/s3/terraform.tfstate"
-    region = "eu-west-2"
+    bucket         = "devops-terraform-state-smihah"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
     dynamodb_table = "dynamodb-state-locking"
-    encrypt = true
+    encrypt        = true
   }
 }
